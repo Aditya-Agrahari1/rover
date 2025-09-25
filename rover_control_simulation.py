9e914e09517e969db340c294b3f161e2ab2e12ee
Aditya A <agrahariaditya005@gmail.com>
2025-09-26 04:44:50 +0530

    Update rover_control_simulation.py

--- rover_control_simulation.py
+++ rover_control_simulation.py
@@ -1,24 +1,30 @@
 from gpiozero import AngularServo
 from time import sleep
 
-# Use GPIO18 (supports hardware PWM)
-servo = AngularServo(18, min_angle=0, max_angle=180, min_pulse_width=0.0005, max_pulse_width=0.0025)
+# GPIO18 with hardware PWM
+servo = AngularServo(
+    18,
+    min_angle=0,
+    max_angle=180,
+    min_pulse_width=0.0005,
+    max_pulse_width=0.0025
+)
 
 while True:
-    # Right 90°
+    # Right (90°)
     servo.angle = 90
     sleep(1)
 
-    # Back to center
-    servo.angle = 45
+    # Back to center (0°)
+    servo.angle = 0
     sleep(1)
 
-    # Left -90° (or  -90 if calibrated)
-    servo.angle = 90
+    # Left (180°)
+    servo.angle = 180
     sleep(1)
 
-    # Back to center
-    servo.angle = 135
+    # Back to center (0°)
+    servo.angle = 0
     sleep(1)
 
     # Wait 2 seconds
